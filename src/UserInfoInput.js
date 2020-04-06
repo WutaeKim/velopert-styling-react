0129f9b68deb2c7fed12f23941dc790f4d08cdeb
wtreasure <wtreasur2@gmail.com>
2020-04-07 05:51:31 +0900

    chapter5

--- src/UserInfoInput.js
+++ src/UserInfoInput.js
@@ -1,23 +1,28 @@
 import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom';
+import './Validation.css';
 
 const UserInfoInput = (props) => {
   const [info, setInfo] = useState({name: '', age: '', sex: '', agree: ''});
   const [goTo, setGoTo] = useState('/');
+  // ref로 사용할 변수를 input element에서 할당하기 위해 선언. const사용불가
+  let ageRef = '';
+  let nameRef = '';
 
   const passToSetInfo = (e) => {
     const nextInfo = {
       ...info,
       [e.target.name]: e.target.value
     };
-    setInfo(nextInfo );
+    setInfo(nextInfo);
   }
 
   const goToOutput = () => {
     props.infoToAppJs(info);
-    setGoTo('/output');
+    nameRef.value === '' ? nameRef.focus() :
+    ageRef.value === '' ? ageRef.focus() : setGoTo('/output');
   }
-  
+
   if (goTo === '/output') return <Redirect to='/output' />;
 
   return (
@@ -27,25 +32,28 @@ const UserInfoInput = (props) => {
         <tbody>
           <tr>
             <td> 회원이름 </td>
-            <td> <input value={info.name} name='name' 
+            <td> <input value={info.name} name='name' ref={ref => {nameRef=ref}} 
                 placeholder='이름을 입력해 주세요' onChange={passToSetInfo} /> </td>
           </tr>
           <tr>
             <td> 나이 </td>
-            <td> <input value={info.age} name='age' 
+            <td> <input value={info.age} name='age' ref={ref => {ageRef=ref}} 
                 placeholder='나이를 입력해 주세요' onChange={passToSetInfo} /> </td>
           </tr>
           <tr>
             <td> 성별 </td>
             <td> <input value={info.sex} name='sex' 
-                placeholder='성별을 입력해 주세요'
+                placeholder='남자는: m, 여자는: f'
                 onChange={passToSetInfo} /> </td>
           </tr>
           <tr>
             <td> 이메일 수신 </td>
             <td> <input value={info.agree} name='agree'
                 placeholder='동의하면: y, 거부하면: n' 
-                onChange={passToSetInfo} /> </td>
+                onChange={passToSetInfo}
+                className={info.agree.length > 1 ? 'failure' :
+                info.agree !=='y' && info.agree !=='n' && info.agree !== ''
+                  ? 'failure' : ''} /> </td>
           </tr>
         </tbody>
       </table>
--- src/UserInfoOutput.js
+++ src/UserInfoOutput.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
-import PropTypes from 'prop-types';
 
 const UserInfoOutput = (props) => {
   const [goTo, setGoTo] = useState('');
