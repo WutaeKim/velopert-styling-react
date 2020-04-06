46cc7b861493f4defad2c2be652f833a4fb041a8
wtreasure <wtreasur2@gmail.com>
2020-04-07 01:24:04 +0900

    chapter3

---
+++ src/App11111.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import UserInfoInput from './UserInfoInput';
+import UserInfoOutput from './UserInfoOutput';
+import { Switch, Route } from 'react-router-dom';
+import EventPractice from './EventPractice'
+
+const App = () => {
+  return (
+    <EventPractice />
+    );
+}
+
+export default App;
--- src/UserInfoInput.js
+++ src/UserInfoInput.js
@@ -4,10 +4,14 @@ import { Redirect } from 'react-router-dom'
 const UserInfoInput = (props) => {
   //const를 사용하면info가 바뀔때마다 컴포넌트가 초기화되어 수정된 info를 한
   //번에 넘기는 법을 모르겠음
-  let [info, setInfo] = useState({name: '', age: '', sex: '', agree: ''});
-  let [goTo, setGoTo] = useState('/');
+  const [info, setInfo] = useState({name: '', age: '', sex: '', agree: ''});
+  const [goTo, setGoTo] = useState('/');
   const passToSetInfo = (e) => {
-    setInfo({ ...info, [e.target.name]: e.target.value});
+    const nextInfo = {
+      ...info,
+      [e.target.name]: e.target.value
+    };
+    setInfo(nextInfo );
     console.log(info)
   }
 
